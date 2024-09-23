0304e5d4d5ead09b977cd41e51671f81c792c0b0
Alisha <ismailovali2006@gmail.com>
2024-09-23 18:11:20 +0300

    first part of the task

--- app/src/main/java/com/example/mad1/MainActivity.java
+++ app/src/main/java/com/example/mad1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mad1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button button2 = findViewById(R.id.button2);
+        TextView textView2 = findViewById(R.id.textView2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView2.setText("Halal Madrid!");
+            }
+        });
+
     }
 }
